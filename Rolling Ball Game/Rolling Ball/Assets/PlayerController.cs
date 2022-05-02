68ca7fb53700d11851dd6b4b28841ab981651997
Aaron Henderson <96578446+STREETSAMURAI221@users.noreply.github.com>
2022-05-02 05:24:44 -0600

    Got the ball rolling.
    I set up a rough prototype that lets you roll a sphere around. If you fall off the edge the level is reset, and I have an end goal that loads the next scene.

---
+++ Rolling Ball Game/Rolling Ball/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float MoveSpeed = 10f;
+    private float xInput;
+    private float zInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        Move();
+    }
+
+    private void ProcessInputs()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        rb.AddForce(new Vector3(xInput, 0f, zInput) * MoveSpeed); 
+    }
+}
